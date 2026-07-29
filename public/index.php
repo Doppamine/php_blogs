bcd8f68921885a447d96147832ddbc4c4ad02541
Beksultan Baimagambetov <bekabis55@gmail.com>
2026-07-29 22:42:32 +0500

    feat: added front controller

--- public/index.php
+++ public/index.php
@@ -1,1 +1,22 @@
 <?php
+
+use App\Core\Router;
+use App\Core\NotFoundException;
+
+$config = require __DIR__ . '/../bootstrap.php';
+$router = new Router();
+$router->get('/', function (): string {
+    return '<h1>Home Page</h1>';
+});
+
+try {
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $method = strtoupper($_SERVER['REQUEST_METHOD']);
+    echo $router->dispatch($method, $uri);
+} catch (NotFoundException $e) {
+    http_response_code(404);
+    echo $e->getMessage();
+} catch (Throwable $e) {
+    http_response_code(500);
+    echo $e->getMessage();
+}
--- src/Core/Router.php
+++ src/Core/Router.php
@@ -1,8 +1,34 @@
 <?php
-
+declare(strict_types=1);
 namespace App\Core;
 
 class Router
 {
+    private array $routes = [];
+
+    // This project will only have GET requests
+    public function get(string $pattern, callable $handler): void {
+        $this->routes[] = [
+            'method'  => 'GET',
+            'regex'   => '#^' . str_replace('\{id\}', '(\d+)', preg_quote($pattern, '#')) . '$#',
+            'handler' => $handler,
+        ];
+    }
+    public function dispatch(string $method, string $path): string {
+        $path = rtrim($path, '/');
+        if ($path === '') {
+            $path = '/';
+        }
+
+        $method = strtoupper($method);
+
+        foreach ($this->routes as $route) {
+            if ($route['method'] === $method && preg_match($route['regex'], $path, $matches)) {
+                array_shift($matches);
+                return ($route['handler'])(...array_map('intval', $matches));
+            }
+        }
+        throw new NotFoundException("Route not found");
+    }
 
 }
